9ca9b246d4951edf4f8a4e8cef795eb8c2dce9e2
Ahmad Saleh <ahmad@kiitos-tech.com>
2022-06-03 14:21:30 +0300

    finishing up the controller of all functionality

---
+++ controller/noteController.js
@@ -0,0 +1,15 @@
+exports.getAllNotes = (req, res) => {
+  res.send("Get All Notes");
+};
+
+exports.saveNote = (req, res) => {
+  res.send("Save Notes");
+};
+
+exports.updateNote = (req, res) => {
+  res.send("update the note");
+};
+
+exports.deleteNote = (req, res) => {
+  res.send("delete the note");
+};
--- route/noteRoute.js
+++ route/noteRoute.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/notes", function (req, res) {
-  res.send("Get All Notes");
-});
+const NotesController = require("../controller/noteController");
+router.get("/notes", NotesController.getAllNotes);
 
 module.exports = router;
