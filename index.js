083a7fc8edce91b53eaf73db73868237da81c554
Ahmad Saleh <ahmad@kiitos-tech.com>
2022-06-03 03:16:25 +0300

    cleaning the code with changing for the node modules

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // setup your server here
 var express = require("express");
 var cors = require("cors");
-var app = express();
 var bodyParser = require("body-parser");
+var app = express();
 const port = 3000;
 
 app.use(cors());
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.send("Welcome to the Express server!");
 });
 
