6be7daf5c80cfcfd75710b8f7b6a2b0dbcf640ce
Ahmad Saleh <ahmad@kiitos-tech.com>
2022-06-03 03:10:02 +0300

    updating and intializing the server

---
+++ index.js
@@ -0,0 +1,19 @@
+// setup your server here
+var server = require("express");
+var cors = require("cors");
+var app = express();
+var bodyParser = require("body-parser");
+const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express server!");
+});
+
+app.listen(port, () => {
+  console.log("the server started .....");
+});
