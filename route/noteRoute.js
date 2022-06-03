2eee32b7e71828a45dadbbe23a4f7d3d1086cc53
Ahmad Saleh <ahmad@kiitos-tech.com>
2022-06-03 14:45:05 +0300

    intializing all routes of note app

--- route/noteRoute.js
+++ route/noteRoute.js
@@ -2,5 +2,8 @@ const express = require("express");
 const router = express.Router();
 const NotesController = require("../controller/noteController");
 router.get("/notes", NotesController.getAllNotes);
+router.post("/notes/save", NotesController.saveNote);
+router.put("/notes/update", NotesController.updateNote);
+router.delete("/notes/delete", NotesController.deleteNote);
 
 module.exports = router;
